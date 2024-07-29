3056c56cc27cb9dedf3f6f5f7d60f768e787b0ef
Herman S <herman.stepanov.lineardev@gmail.com>
2024-07-29 23:32:14 +0300

    change link

--- src/contentful/configData.ts
+++ src/contentful/configData.ts
@@ -1,11 +1,15 @@
 import { BLOCKS } from '@contentful/rich-text-types';
 
-export const locales = ['en-US','uk']
+export const locales = ['en-US', 'uk'];
 
 export const options = {
-    renderNode: {
-      //@ts-ignore
-        [BLOCKS.EMBEDDED_ASSET]: ({ data: { target: { fields }}}) =>
-            `<img src="${fields.file.url}" height="${fields.file.details.image.height}" width="${fields.file.details.image.width}" alt="${fields.description}"/>`,
+  renderNode: {
+    //@ts-ignore
+    [BLOCKS.EMBEDDED_ASSET]: ({ data: { target: { fields } } }) => {
+      if (fields && fields.file && fields.file.details && fields.file.details.image) {
+        return `<img src="${fields.file.url}" height="${fields.file.details.image.height}" width="${fields.file.details.image.width}" alt="${fields.description}" />`;
+      }
+      return '';
     },
+  },
 };
